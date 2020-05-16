a51dde7aa062fd2416ffaa13cac5b939f0ef9fc2
Craig McLeod <mcleod.craig@outlook.com>
2020-05-16 19:42:09 +0100

    Seventeenth and eighteenth exercise

--- pet_shop.rb
+++ pet_shop.rb
@@ -69,4 +69,10 @@ end
 def add_pet_to_customer (customer, new_pet)
     customer[:pets].push(new_pet)
 end
+
+def customer_can_afford_pet (customer, new_pet)
+    return customer[:cash] >= new_pet[:price]
+end
+
+
         
