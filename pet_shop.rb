0045849021bfe64638e29c09d178337449a10f3f
Craig McLeod <mcleod.craig@outlook.com>
2020-05-16 07:46:21 +0100

    Sixth exercise

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,19 @@
 def pet_shop_name (pet_shop)
-        return pet_shop[:name]
+    return pet_shop[:name]
+end
+
+def total_cash (pet_shop)
+    return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash (pet_shop, cash)
+    pet_shop[:admin][:total_cash] += cash
+end
+
+def pets_sold (pet_shop)
+   return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, number_of_pets_sold)
+    pet_shop[:admin][:pets_sold] += number_of_pets_sold
 end
